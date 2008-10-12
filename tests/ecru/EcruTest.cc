406d2b65938c6e16da5960cd215b4ef062441841
Roman Bogorodskiy <bogorodskiy@gmail.com>
2008-10-12 14:05:31 +0400

    Test improvements.

--- tests/ecru/EcruTest.cc
+++ tests/ecru/EcruTest.cc
@@ -24,13 +24,37 @@ CPPUNIT_TEST_SUITE_REGISTRATION( EcruTest );
 
 void EcruTest::testStripString()
 {
+	// typical usage
 	string unstripped = "   somestr ";
 	string stripped = ecru::stripString(unstripped);
 
 	CPPUNIT_ASSERT_EQUAL(unstripped.size(), stripped.size() + 4);
+	CPPUNIT_ASSERT(stripped == "somestr");
+
+	// find out what's going on if we pass empty string
+	string empty = ecru::stripString("");
+
+	CPPUNIT_ASSERT(empty == "");
+
+	// check if it doesn't modify a string that doesn't need 
+	// to be stripped
+	string alreadyStripped;
+	string doubleStrip = ecru::stripString(alreadyStripped);
+	
+	CPPUNIT_ASSERT(doubleStrip == alreadyStripped);
 }
 
 void EcruTest::testStripNewLines()
 {
-	CPPUNIT_ASSERT_EQUAL(1, 1);
+	// typical usage
+	string stringWithNewLines = "hello\nhow are\nyou doing\n";
+	string stripped = ecru::stripNewLines(stringWithNewLines);
+	
+	CPPUNIT_ASSERT(stripped.find("\n") == string::npos);
+
+	// empty
+	CPPUNIT_ASSERT("" == ecru::stripNewLines(""));
+
+	// without new lines
+	CPPUNIT_ASSERT("foo" == ecru::stripNewLines("foo"));
 }
